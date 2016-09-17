4844997d39e826a89ca8ca140196e3f37d52b59d
Denis Carriere <carriere.denis@gmail.com>
2016-09-17 16:30:06 -0400

    Add padded zero fix file

--- octranspo/pad-zero.ts
+++ octranspo/pad-zero.ts
@@ -6,17 +6,19 @@ const features: GeoJSON.Feature<any>[] = []
 
 stops.features.map(stop => {
     const ref: string = stop.properties.ref
-    if (ref.length === 4) {}
-    else if (ref.length === 3) {
-        stop.properties.ref = `0${ ref }`
-        console.log('WARNING - Padded reference number:', stop.properties.name)
-    } else {
-        stop.properties.ref = null
-        stop.properties['source:ref'] = null
-        stop.properties.fixme = 'No reference number'
-        console.log('WARNING - No Reference number:', stop.properties.name)
+    if (ref) {
+        if (ref.length === 4) {}
+        else if (ref.length === 3) {
+            stop.properties.ref = `0${ ref }`
+            features.push(stop)
+            console.log('WARNING - Padded reference number:', stop.properties.name)
+        } else {
+            stop.properties.ref = null
+            stop.properties['source:ref'] = null
+            stop.properties.fixme = 'No reference number'
+            console.log('WARNING - No Reference number:', stop.properties.name)
+        }
     }
-    features.push(stop)
 })
 
-fs.writeFileSync('oc-transpo-stops.json', JSON.stringify(turf.featureCollection(features), null, 4))
+fs.writeFileSync('oc-transpo-stops-pad.json', JSON.stringify(turf.featureCollection(features), null, 4))
