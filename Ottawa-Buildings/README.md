faedacac730df85bb26563500fc7c466455b1b63
Denis Carriere <carriere.denis@gmail.com>
2016-10-01 00:56:42 -0400

    Add Ottawa building get data script

---
+++ Ottawa-Buildings/README.md
@@ -0,0 +1,32 @@
+### Status of Completion (0%)
+
+- **[Building footprint (0 / 310000)]()**
+
+### Validation
+
+### Mapillary
+
+- **[Add photos to buildings (0 / 310000)]()**
+
+## Quickstart
+
+Step 1. Select a Tile & Start Mapping
+
+Step 2: Edit with JOSM
+
+Step 3: Apply an inverse filter to find any existing data`"addr:street"`=* or `-building`=*
+
+Step 4: **Download data** - Found in the Extra Instructions
+
+Step 5: Add feature or merge new data with existing features (Don't delete features).
+
+## Workflow:
+
+- Edit with JOSM, check for any existing buildings that might create conflicts (Delete or merge)
+- Download Building (new layer)
+- Download Missing Address (merge address data to building using JOSM address plugin)
+- Click JOSM validate and fix any errors or warnings.
+
+## Per Task Instructions
+
+#### [**Click to Download - Building Footprint**](http://localhost:8111/import?new_layer=true&url=https://localhost:5000/datasets/{z}/{x}/{y}/ottawa-buildings.osm)
--- Ottawa-Buildings/get-data.ts
+++ Ottawa-Buildings/get-data.ts
@@ -29,8 +29,15 @@ async function main() {
   const writer = fs.createWriteStream(path.join(__dirname, 'buildings.json'))
   const api_url = 'http://maps.ottawa.ca/arcgis/rest/services/TopographicMapping/MapServer/3/query'
   let count = 0
+  let first = false
   const max = 250
   const features: Array<GeoJSON.Feature<GeoJSON.Polygon>> = []
+
+  // Write Feature Collection Header
+  writer.write(`{
+  "type": "FeatureCollection",
+  "features": [
+`)
 
   while (true) {
     const params = {
@@ -51,21 +58,25 @@ async function main() {
       .then(data => JSON.parse(data))
     r.features.map(feature => {
       const attributes = {
-        ref: feature.attributes.OBJECTID,
         building: 'yes',
-        source: 'City of Ottawa',
-        'source:ref': 'City of Ottawa'
+        source: 'City of Ottawa'
       }
       const poly = turf.polygon(feature.geometry.rings, attributes)
-      features.push(poly)
+      if (!first) {
+        writer.write(`    ${ JSON.stringify(poly)}`)
+        first = true
+      } else writer.write(`,\n    ${ JSON.stringify(poly)}`)
     })
 
     // Stop or Start over
     if (!r.features.length) break
-    console.log(features.length)
+    console.log(count)
     count += max
-    //if (count > 200000) break
+
+    // if (count > 1000) break
   }
-  writer.write(JSON.stringify(turf.featureCollection(features), null, 4))
+  writer.write(`
+  ]
+}`)
 }
 main()
