283b0af1b184fb635a8fb08bb9e0676d2166481e
zzptichka <shkvorets@gmail.com>
2018-12-10 23:09:14 -0500

    working script

--- StopYieldDirections/main.js
+++ StopYieldDirections/main.js
@@ -1,5 +1,10 @@
 const reader = require('geojson-writer').reader
 const ruler = require('cheap-ruler')(45.41, 'meters')
+const xmldom = require('xmldom').DOMParser
+var XMLSerializer = require('xmldom').XMLSerializer;
+const fs = require('fs');
+
+//first go through osmose-generated json with ways/signs and figure out sign directions
 
 const forwards = [];
 const backwards = [];
@@ -55,3 +60,49 @@ for(let road of roads.features){
 
 console.log(`Found ${forwards.length} forwards signs`);
 console.log(`Found ${backwards.length} backwards signs`);
+
+//now process osm file and add forward/backward tags
+
+const inOsmSource = "roadsWithSigns.osm"
+const outOsmSource = "roadsWithSignsAndDirections.osm"
+
+const serializer = new XMLSerializer();
+let totalTagged = 0
+
+
+fs.readFile(inOsmSource, 'utf-8', function (err, data) {
+  if (err) {
+    throw err;
+  }
+
+  const doc = new xmldom().parseFromString(data, 'application/xml');
+  const nodes = doc.getElementsByTagName('node');
+  loop1:
+  for (let i in nodes) {
+    const node = nodes[i];
+    for(let j in node.attributes){
+      const attr = node.attributes[j];
+      if (attr.name=='id') {
+        const id = 'node/'+attr.value;
+        const direction = forwards.includes(id) ? 'forward' : (backwards.includes(id) ? 'backward' : '');
+        if(direction=='') continue loop1;
+
+        const tag = doc.createElement("tag");
+        tag.setAttribute('k', 'direction')
+        tag.setAttribute('v', direction);
+        node.appendChild(tag)
+        node.setAttribute('action', 'modify');
+      //  console.log('For', id, 'created sidewalk tag with', direction)
+        totalTagged++
+
+      }
+    }
+  }
+
+  fs.writeFile(outOsmSource, serializer.serializeToString(doc), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("Saved! Total tagged signs:", totalTagged);
+  });
+});
