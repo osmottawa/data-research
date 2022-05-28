cd50a465191dc7865fa9ca59ee0a9983296f3e95
YaroShkvorets <shkvorets@gmail.com>
2022-05-28 18:30:02 -0400

    update parking lots

--- Ottawa-parks-parking/clean.js
+++ Ottawa-parks-parking/clean.js
@@ -3,41 +3,51 @@ const turf = require("@turf/turf");
 const fs = require("fs");
 const geojson2osm = require('geojson2osm');
 const reader = require('geojson-writer').reader
+const rbush = require('geojson-rbush')
 
-const collection1 = turf.featureCollection([]);
-const collection2 = turf.featureCollection([]);
+const   oldTree = rbush(),
+        newTree = rbush(),
+        newCircles = rbush()
 
-let pools = reader('parking-city-parks-source.json')
+const oldFeatures = reader('parking-city-parks-source_2017-12.json')
+const newFeatures = reader('parking-city-parks-source_2022-05.geojson')
 
-pools.features.map(result => {
+oldFeatures.features.forEach(place => {
+    const point = turf.point(place.geometry.coordinates);
+    oldTree.insert(point)
+});
 
-    const fullroad = result.properties.ADDRESS;
-    let housenumber = fullroad.match(/^\d[\d\-a-zA-Z]*/) ? fullroad.match(/^\d[\d\-a-zA-Z]*/)[0] : undefined;
+
+newFeatures.features.forEach(place => {
+
+    console.log(`Processing #${place.properties.FACILITYID}`)
+    if(!place.geometry) return;
+    const fullroad = place.properties.ADDRESS;
+    const housenumber = fullroad?.match(/^\d[\d\-a-zA-Z]*/)[0] ?? undefined;
     let street = housenumber ? fullroad.replace(housenumber, '').trim().replace(/^,/, '').trim().replace(/,$/, '').trim() : undefined;
-    street = street.replace(/ ave /ig,' Avenue, ').replace(/ rd /ig,' Road, ').replace(/ st /ig,' Street, ').replace(/ dr /ig,' Drive, ').replace(/ ave /ig,' Boulevard, ').replace(/ crt /ig,' Crescent, ').replace(/ lane /ig,' Lane, ').replace(/ cres /ig,' Crescent, ').replace(/ drive /ig,' Drive, ').replace(/ avenue /ig,' Avenue, ').replace(/ pl /ig,' Place, ').replace(/ way /ig,' Way, ').replace(/ crescent /ig,' Crescent, ').replace(/ road /ig,' Road, ').replace(/ blvd /ig,' Boulevard, ').replace(/ ter /ig,' Terrace, ').replace(/ PKWY /ig,' Parkway, ').replace(/ HWY 174 /ig,' Highway 174, ');
-    street = street.replace('E.','East').replace('N.','North').replace('W.','West').replace('S.','South');
-    if (street && street.match(',')) {
+    street = street?.replace(/ ave /ig,' Avenue, ').replace(/ rd /ig,' Road, ').replace(/ st /ig,' Street, ').replace(/ dr /ig,' Drive, ').replace(/ ave /ig,' Boulevard, ').replace(/ crt /ig,' Crescent, ').replace(/ lane /ig,' Lane, ').replace(/ cres /ig,' Crescent, ').replace(/ drive /ig,' Drive, ').replace(/ avenue /ig,' Avenue, ').replace(/ pl /ig,' Place, ').replace(/ way /ig,' Way, ').replace(/ crescent /ig,' Crescent, ').replace(/ road /ig,' Road, ').replace(/ blvd /ig,' Boulevard, ').replace(/ ter /ig,' Terrace, ').replace(/ PKWY /ig,' Parkway, ').replace(/ HWY 174 /ig,' Highway 174, ');
+    street = street?.replace('E.','East').replace('N.','North').replace('W.','West').replace('S.','South');
+    if (street?.match(',')) {
         street = street.split(',')[0];
     }
 
     //capitalize 1st letter of each word
-    street = street.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    street = street?.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 
-    const ref = result.properties.FACILITYID
-    const capacity = result.properties.CAPACITY
-    const disabled = result.properties.ACCESSIBLE
-    const lit = result.properties.LIGHTING?(result.properties.LIGHTING.match(/yes/g) ? 'yes' : 'no'):'no';
-    const fee = result.properties.PAID_PARKI?(result.properties.PAID_PARKI.match(/yes/g) ? 'yes' : 'no'):'no';
-    const surface = result.properties.SURFACE
+    const ref = place.properties.FACILITYID
+    const capacity = place.properties.CAPACITY
+    const disabled = place.properties.ACCESSIBLE_PARKING_CAPACITY ?? ''
+    const lit = place.properties.LIGHTING?(place.properties.LIGHTING.match(/yes/g) ? 'yes' : 'no'):'no';
+    const fee = place.properties.PAID_PARKI?(place.properties.PAID_PARKI.match(/yes/g) ? 'yes' : 'no'):'no';
+    const surface = place.properties.SURFACE
 
     const properties = {
         ref,
-        'name': 'Parking Lot',
         'amenity': 'parking',
         'parking': 'surface',
         'access': 'permissive',
-        'addr:housenumber': housenumber,
-        'addr:street': street,
+        'addr:housenumber': housenumber ?? '',
+        'addr:street': street ?? '',
         'capacity': capacity,
         'capacity:disabled': disabled,
         'opening_hours': '06:00-23:00',
@@ -47,17 +57,25 @@ pools.features.map(result => {
         'fee': fee,
         'operator': 'City of Ottawa',
         'source': 'City of Ottawa',
-        'source:date': '2017-12-28',
+        'source:date': '2022-05-28',
         'ref': ref
     };
-    console.log("Parking #", collection1.features.length+1, '@', housenumber, street);
-    const point = turf.point(result.geometry.coordinates, properties);
-    collection1.features.push(point);
+    const point = turf.point(place.geometry.coordinates, properties);
+    let nearby = oldTree.search(turf.circle(point.geometry.coordinates, 1, 10, 'meters')).features  //check if there was an old one within 1m
+    if(nearby.length){
+        console.log(`Duplicate @ ${housenumber}, ${street}`);
+        return;
+    }
 
-    var circle = turf.circle(result.geometry.coordinates, 200, 10, 'meters');
-    collection2.features.push(circle)
+    console.log(`New Feature @ ${housenumber}, ${street}`,);
+
+    const circle = turf.circle(point, 100, 10, 'meters');
+    newCircles.insert(circle);
+    newTree.insert(point)
 });
 
-const osm = geojson2osm.geojson2osm(collection1)
+console.log('Circles: ',newCircles.all().features.length, 'Features: ',newTree.all().features.length)
+
+const osm = geojson2osm.geojson2osm(newTree.all())
 fs.writeFileSync('parks-parking.osm', osm);
-fs.writeFileSync('parks-parking-circles.geojson', JSON.stringify(collection2, null, 4));
+fs.writeFileSync('parks-parking-circles.geojson', JSON.stringify(newCircles.all(), null, 4));
